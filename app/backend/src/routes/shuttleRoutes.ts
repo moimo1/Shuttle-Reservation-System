790c86c04e02ab4a7405ef8a620ebad148920d95
Renz Dadpaas <rdadpaas123@gmail.com>
2025-12-04 15:42:05 +0800

    added search shuttle based on departure time

--- app/backend/src/routes/shuttleRoutes.ts
+++ app/backend/src/routes/shuttleRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { getShuttles, reserveShuttle } from "../controllers/shuttleController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
+import { searchShuttles } from "../controllers/searchController.js";
 
 const router = express.Router();
 
 router.get("/", getShuttles);
+router.get("/search", searchShuttles);
 router.post("/reserve/:shuttleId", authMiddleware, reserveShuttle);
 
 export default router;
