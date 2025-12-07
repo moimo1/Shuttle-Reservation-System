b0c0c80f7cbeb9b05feb1ae15c251327202e4609
Dannievic <2240879>
2025-12-07 18:42:21 +0800

    Added MapScreen (android only)

---
+++ app/frontend/assets/markers.ts
@@ -0,0 +1,17 @@
+export const markers = [
+	// Sample markers
+	{
+        latitude: 16.4076,
+        longitude: 120.5850,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+		name: 'Saint Louis University (Main)'
+	},
+	{
+        latitude: 16.3965,
+        longitude: 120.5992,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+		name: 'Saint Louis University (Maryheights)'
+	}
+];
--- app/frontend/src/screens/MapScreen.tsx
+++ app/frontend/src/screens/MapScreen.tsx
@@ -1,12 +1,85 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useRef } from 'react';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE, Region } from 'react-native-maps';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { markers } from '../../assets/markers';
 
-export default function ViewMapScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Map View (Blank)</Text>
-    </View>
-  );
+const INITIAL_REGION = {
+	latitude: 16.4023,
+	longitude: 120.5960,
+	latitudeDelta: .01,
+	longitudeDelta: .01
+};
+
+export default function App() {
+	const mapRef = useRef<any>(null);
+	const navigation = useNavigation();
+
+	useEffect(() => {
+		navigation.setOptions({
+			headerRight: () => (
+				<TouchableOpacity onPress={focusMap}>
+					<View style={{ padding: 10 }}>
+						<Text>Focus</Text>
+					</View>
+				</TouchableOpacity>
+			)
+		});
+	}, []);
+
+  // Animation sample
+	const focusMap = () => {
+		const SluMaryheights = {
+        latitude: 16.3965,
+        longitude: 120.5992,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+		};
+
+		mapRef.current?.animateToRegion(SluMaryheights);
+		// mapRef.current?.animateCamera({ center: SluMaryheights, zoom: 10 }, { duration: 2000 });
+	};
+
+	const onMarkerSelected = (marker: any) => {
+		Alert.alert(marker.name);
+	};
+
+	const calloutPressed = (ev: any) => {
+		console.log(ev);
+	};
+
+	const onRegionChange = (region: Region) => {
+		console.log(region);
+	};
+
+	return (
+		<View style={{ flex: 1 }}>
+			<MapView
+				style={StyleSheet.absoluteFillObject}
+				initialRegion={INITIAL_REGION}
+				showsUserLocation
+				showsMyLocationButton
+				provider={PROVIDER_GOOGLE}
+				ref={mapRef}
+				onRegionChangeComplete={onRegionChange}
+			>
+				{markers.map((marker, index) => (
+					<Marker
+						key={index}
+						title={marker.name}
+						coordinate={marker}
+						onPress={() => onMarkerSelected(marker)}
+					>
+						<Callout onPress={calloutPressed}>
+							<View style={{ padding: 10 }}>
+								<Text style={{ fontSize: 24 }}>Hello</Text>
+							</View>
+						</Callout>
+					</Marker>
+				))}
+			</MapView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
